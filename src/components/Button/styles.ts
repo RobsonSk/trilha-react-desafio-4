add988fbbefe569260aa504c7be0166ce5ad650f
Robson Scartezini <146673320+RobsonSk@users.noreply.github.com>
2024-08-31 15:43:35 -0300

    Resolução desafio

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
 
-export const ButtonContainer = styled.button`
+export const ButtonContainer = styled.button<{
+    children: React.ReactNode;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    disabled?: boolean;
+}>`
     width: 100%;
     height: 42px;
     background-color: #81259D;
@@ -13,5 +17,10 @@ export const ButtonContainer = styled.button`
     &:hover {
         opacity: 0.6;
         cursor:pointer;
+    }
+       
+    &:disabled {
+        opacity: 0.5;
+        cursor: not-allowed;
     }
 `
